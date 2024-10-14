c636cea0720eab15481e96d675aeb4e6ee69c54d
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-10-15 01:04:12 +0530

    Dynamic and nested Dynamic Routes

---
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,13 @@
+export default function ReviewDetails({
+  params,
+}: {
+  params: { productId: string; reviewId: string };
+}) {
+  return (
+    <>
+      <h1>
+        Review {params.reviewId} for Product {params.productId}
+      </h1>
+    </>
+  );
+}
