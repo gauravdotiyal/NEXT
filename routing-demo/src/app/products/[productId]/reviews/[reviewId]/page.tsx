87a10b876e804073957ac553feb3793abbb759c1
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-10-15 12:08:25 +0530

    Not Found Page Handling

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetails({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }  
   return (
     <>
       <h1>
@@ -11,4 +16,3 @@ export default function ReviewDetails({
     </>
   );
 }
- 
