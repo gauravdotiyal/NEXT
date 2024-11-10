0beb602f93409565745b949288dcc33a86cc82aa
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-11-11 01:40:23 +0530

    Query Parameters and Route Handlers

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,7 +1,14 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
-  return Response.json(comments);
+//query parameter is used for search sort and pagination functionality
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+  return Response.json(filteredComments);
 }
 
 export async function POST(request: Request) {
