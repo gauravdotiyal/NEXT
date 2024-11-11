abd0d54b62bc37c2801c7ba45fef3d38cb2b8540
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-11-11 11:18:18 +0530

    Cookies and Caching in Route Handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,17 +1,27 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers,cookies } from "next/headers";
 
 
 export async function GET(request:NextRequest){
     const requestHeaders=new Headers(request.headers);
     const headerList=headers();
+    cookies().set("resultsPerPage","20");
 
+    
+    const theme=request.cookies.get("theme");
+
+    // Headers
     console.log(requestHeaders.get("Authorization"));
     console.log(headerList.get("Authorization"));
+    
+    // cookies
+    console.log(theme);
+    console.log(cookies().get("resultsPerPage"));
 
     return new Response("<h1>Profile Api Data</h1>",{
         headers:{
             "Content-Type":"text/html",
+            "Set-Cookie":"theme=dark"
         }
     });
 }
