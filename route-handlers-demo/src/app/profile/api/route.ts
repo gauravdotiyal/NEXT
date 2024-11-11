c48e7b5852b2485c472594311440a11cdb7bed8d
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-11-11 10:40:58 +0530

    Headers and their content type

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,17 @@
-export async function GET(){
-    return new Response("Profile API Data");
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+
+export async function GET(request:NextRequest){
+    const requestHeaders=new Headers(request.headers);
+    const headerList=headers();
+
+    console.log(requestHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"));
+
+    return new Response("<h1>Profile Api Data</h1>",{
+        headers:{
+            "Content-Type":"text/html",
+        }
+    });
 }
