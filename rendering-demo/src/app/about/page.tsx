46e9ceef0136d3347c64a7bcf96fb84424bd06bd
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-11-13 16:19:23 +0530

    Static Rendering and Dynamic Rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,5 +1,9 @@
+import { cookies } from "next/headers";
 
-export default function Page(){
-    console.log('About Server Components')
-    return <h1>Page</h1>
+export default async function AboutPage() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
+  console.log("About Server Components");
+  return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 }
